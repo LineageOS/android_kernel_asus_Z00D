e4d17473638d5dc4709effa8e64e8d2b674c15fe
Al Viro <viro@zeniv.linux.org.uk>
2013-10-04 11:06:42 -0400

    get rid of s_files and files_lock
    commit eee5cc2702929fd41cce28058dc6d6717f723f87 upstream.
    
    The only thing we need it for is alt-sysrq-r (emergency remount r/o)
    and these days we can do just as well without going through the
    list of files.
    
    Change-Id: Ic4e3cae74752b270914741d8e5dbd1fa8cf63f03
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    [wangkai: backport to 3.10: adjust context]
    Signed-off-by: Wang Kai <morgan.wang@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/super.c
+++ fs/super.c
@@ -157,19 +157,15 @@ static struct super_block *alloc_super(struct file_system_type *type, int flags)
 		if (security_sb_alloc(s))
 			goto out_free_sb;
 
-#ifdef CONFIG_SMP
-		s->s_files = alloc_percpu(struct list_head);
-		if (!s->s_files)
-			goto err_out;
-		else {
-			int i;
-
-			for_each_possible_cpu(i)
-				INIT_LIST_HEAD(per_cpu_ptr(s->s_files, i));
+		if (security_sb_alloc(s)) {
+			/*
+			 * We cannot call security_sb_free() without
+			 * security_sb_alloc() succeeding. So bail out manually
+			 */
+			kfree(s);
+			s = NULL;
+			goto out;
 		}
-#else
-		INIT_LIST_HEAD(&s->s_files);
-#endif
 		if (init_sb_writers(s, type))
 			goto err_out;
 		s->s_flags = flags;
