7a5dc8e22af8dd12f8804d287d799e5186a364d6
LuK1337 <priv.luk@gmail.com>
2016-12-06 13:48:29 +0100

    input: ftxxxx: Return proper gesture_mode value
    Change-Id: I41ff6f76cf3cbe038110aa0b88e85a2678e0c9bc

--- drivers/input/touchscreen/ftxxxx_ex_fun.c
+++ drivers/input/touchscreen/ftxxxx_ex_fun.c
@@ -934,15 +934,7 @@ static ssize_t switch_gesture_mode_store(struct device *dev, struct device_attri
 
 static ssize_t switch_gesture_mode_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-	bool tmp = 0;
-
-	tmp = (ftxxxx_ts->gesture_mode_type >> 6) & 1;
-
-	if (!tmp)
-		return sprintf(buf, "%x \n", tmp);
-	else
-		return sprintf(buf, "%x \n", ftxxxx_ts->gesture_mode_type);
-
+	return sprintf(buf, "%d\n", ftxxxx_ts->gesture_mode_type);
 }
 
 static ssize_t enable_proximity_check_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
