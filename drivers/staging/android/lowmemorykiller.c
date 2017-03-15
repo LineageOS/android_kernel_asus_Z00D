c6d37ec795e79be0d2d7e328dba5e21b0770b54a
Flex1911 <dedsa2002@gmail.com>
2017-03-15 20:59:01 +0300

    Revert "added Read_TOTAL_DRAM and fixed lmk"
    This reverts commit db390fc1d2bcf5888df3805649ab08b0b5805cf2.
    
    Change-Id: I18cdf073af71a167333f50471e9afd973b836028

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -40,11 +40,13 @@
 #include <linux/swap.h>
 #include <linux/rcupdate.h>
 #include <linux/notifier.h>
+#include <linux/proc_fs.h>
 
 #define CREATE_TRACE_POINTS
 #include "trace/lowmemorykiller.h"
 
 static uint32_t lowmem_debug_level = 1;
+unsigned long lmk_sigkill_cnt;
 static short lowmem_adj[6] = {
 	0,
 	1,
@@ -68,11 +70,42 @@ static unsigned long lowmem_deathpending_timeout;
 			pr_info(x);			\
 	} while (0)
 
-static int is4gDram = 0;
-extern int Read_TOTAL_DRAM(void);
-
+#define DTaskMax 4
+struct task_node {
+     struct list_head node;
+     struct task_struct *pTask;
+};
+typedef enum EListAllocate {
+	EListAllocate_Init,
+	EListAllocate_Head,
+	EListAllocate_Body,
+	EListAllocate_Tail
+};
+static int list_insert(struct task_struct *pTask, struct list_head *pPosition)
+{
+	int nResult = 0;
+	struct task_node *pTaskNode;
+	pTaskNode = kmalloc(sizeof(struct task_node), GFP_KERNEL);
+	if (pTaskNode) {
+		pTaskNode->pTask = pTask;
+		list_add(&pTaskNode->node, pPosition);
+		nResult = 1;
+	}
+	return nResult;
+}
+static void list_reset(struct list_head *pList)
+{
+	struct task_node *pTaskIterator;
+	struct task_node *pTaskNext;
+	list_for_each_entry_safe(pTaskIterator, pTaskNext, pList, node) {
+		list_del(&pTaskIterator->node);
+		kfree(pTaskIterator);
+	}
+}
 static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 {
+	LIST_HEAD(ListHead);
+	int nTaskNum = 0;
 	struct task_struct *tsk;
 	struct task_struct *selected = NULL;
 	int rem = 0;
@@ -86,10 +119,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	int nr_swap_pages = get_nr_swap_pages();
 	int other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages + nr_swap_pages;
 	int other_file = global_page_state(NR_FILE_PAGES) -
-						global_page_state(NR_SHMEM);
-
-	int dma32_free = 0, dma32_file = 0;
-	struct zone *zone;
+						global_page_state(NR_SHMEM) - total_swapcache_pages();;
 
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
@@ -106,36 +136,6 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		lowmem_print(3, "lowmem_shrink %lu, %x, ofree %d %d, ma %hd\n",
 				sc->nr_to_scan, sc->gfp_mask, other_free,
 				other_file, min_score_adj);
-
-	if(is4gDram == 1) {
-		if (min_score_adj == OOM_SCORE_ADJ_MAX + 1) {
-			/*
-			 * Shrink callback is called but global memory is ok. Maybe
-			 * DMA32 zone memory is in short.
-			 */
-			for_each_populated_zone(zone) {
-				if (is_dma32(zone)) {
-					dma32_free = zone_page_state(zone, NR_FREE_PAGES);
-					dma32_file = zone_page_state(zone, NR_FILE_PAGES)
-						- zone_page_state(zone, NR_SHMEM);
-				}
-			}
-
-		for (i = 0; i < array_size/2; i++) {
-				minfree = lowmem_minfree[i];
-				if (dma32_free && dma32_free < minfree
-						&& dma32_file < minfree) {
-					min_score_adj = lowmem_adj[i];
-					lowmem_print(1, "dma32 is in short: free:%d, file:%d, min_score_adj:%hu\n",
-						dma32_free, dma32_file, min_score_adj);
-					break;
-				}
-			}
-
-			lowmem_print(3, "dma32: free:%d, file:%d, min_score_adj:%hu\n",
-					dma32_free, dma32_file, min_score_adj);
-		}
-	}
 	rem = global_page_state(NR_ACTIVE_ANON) +
 		global_page_state(NR_ACTIVE_FILE) +
 		global_page_state(NR_INACTIVE_ANON) +
@@ -162,6 +162,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		if (test_tsk_thread_flag(p, TIF_MEMDIE)) {
 			if (time_before_eq(jiffies,
 				lowmem_deathpending_timeout)) {
+				list_reset(&ListHead);
 				task_unlock(p);
 				rcu_read_unlock();
 				return 0;
@@ -179,19 +180,115 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		task_unlock(p);
 		if (tasksize <= 0)
 			continue;
-		if (selected) {
-			if (oom_score_adj < selected_oom_score_adj)
-				continue;
-			if (oom_score_adj == selected_oom_score_adj &&
-			    tasksize <= selected_tasksize)
-				continue;
+
+		lowmem_print(3, "[Candidate] comm: %s, oom_score_adj: %d, tasksize: %d\n", p->comm, oom_score_adj, tasksize);
+		if (nTaskNum == 0) {
+			if (list_insert(p, &ListHead))
+				nTaskNum++;
+			else
+				lowmem_print(1, "Unable to allocate memory (%d)\n", EListAllocate_Init);
+		} else {
+			/* Find the node index that fit for current task */
+			struct task_node *pTaskSearchIterator;
+			struct task_node *pTaskSearchNext;
+			list_for_each_entry_safe(pTaskSearchIterator, pTaskSearchNext, &ListHead, node) {
+				struct task_struct *pTask;
+				pTask = pTaskSearchIterator->pTask;
+				if (!pTask)
+					continue;
+				task_lock(pTask);
+				int nTargeteAdj = 0;
+				int nTargetSize = 0;
+				if (pTask->signal)
+					nTargeteAdj = pTask->signal->oom_score_adj;
+				if (pTask->mm)
+					nTargetSize = get_mm_rss(pTask->mm);
+				task_unlock(pTask);
+				if (oom_score_adj > nTargeteAdj) {
+				} else if (oom_score_adj < nTargeteAdj) {
+					break;
+				} else {
+					if (tasksize > nTargetSize) {
+					} else if (tasksize <= nTargetSize) {
+						break;
+					}
+				}
+			}
+
+			int nError = 0;
+			struct list_head *pInsertPos = NULL;
+			/* Determine the insert position */
+			if (&pTaskSearchIterator->node == &ListHead) {
+				/* Add node to tail */
+				pInsertPos = ListHead.prev;
+				nError = EListAllocate_Tail;
+			} else if (&pTaskSearchIterator->node == ListHead.next) {
+				if (nTaskNum < DTaskMax) {
+					/* Add node to head */
+					pInsertPos = &ListHead;
+					nError = EListAllocate_Head;
+				}
+			} else {
+				/* Insert node to the list */
+				pInsertPos = pTaskSearchIterator->node.prev;
+				nError = EListAllocate_Body;
+			}
+			/* Perform insertion */
+			if (pInsertPos) {
+				if (list_insert(p, pInsertPos))
+					nTaskNum++;
+				else
+					lowmem_print(1, "Unable to allocate memory (%d)\n", nError);
+			}
+			/* Delete node if the kept tasks exceed the limit */
+			if (nTaskNum > DTaskMax) {
+				struct task_node *pDeleteNode = list_entry((ListHead).next, struct task_node, node);
+				list_del((ListHead).next);
+				kfree(pDeleteNode);
+				nTaskNum--;
+			}
 		}
-		selected = p;
-		selected_tasksize = tasksize;
-		selected_oom_score_adj = oom_score_adj;
-		lowmem_print(2, "select '%s' (%d), adj %hd, size %d, to kill\n",
-			     p->comm, p->pid, oom_score_adj, tasksize);
+		/*
+		struct task_node *pTaskIterator;
+		struct task_node *pTaskNext;
+		list_for_each_entry_safe(pTaskIterator, pTaskNext, &ListHead, node) {
+			char *szName = "NULL";
+			int nAdj = 0;
+			int nSize = 0;
+			struct task_struct *pTask;
+			pTask = pTaskIterator->pTask;
+			if (!pTask)
+				continue;
+			task_lock(pTask);
+			if (pTask->comm)
+				szName = pTask->comm;
+			if (pTask->signal)
+				nAdj = pTask->signal->oom_score_adj;
+			if (pTask->mm)
+				nSize = get_mm_rss(pTask->mm);
+			lowmem_print(3, "[Current] comm: %s, oom_score_adj: %d, tasksize: %d\n", pTask->comm, nAdj, nSize);
+			task_unlock(pTask);
+		}*/
+
 	}
+
+	struct task_node *pTaskIterator;
+	struct task_node *pTaskNext;
+	list_for_each_entry_safe_reverse(pTaskIterator, pTaskNext, &ListHead, node) {
+		selected = pTaskIterator->pTask;
+		if (!selected)
+			continue;
+		task_lock(selected);
+		if (test_tsk_thread_flag(selected, TIF_MEMDIE)) {
+			task_unlock(selected);
+			continue;
+		}
+		if (selected->signal)
+			selected_oom_score_adj = selected->signal->oom_score_adj;
+		if (selected->mm)
+			selected_tasksize = get_mm_rss(selected->mm);
+		task_unlock(selected);
+
 	if (selected) {
 		long cache_size = other_file * (long)(PAGE_SIZE / 1024);
 		long cache_limit = minfree * (long)(PAGE_SIZE / 1024);
@@ -210,9 +307,11 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			     free);
 		lowmem_deathpending_timeout = jiffies + HZ;
 		send_sig(SIGKILL, selected, 0);
+		lmk_sigkill_cnt++;
 		set_tsk_thread_flag(selected, TIF_MEMDIE);
 		rem -= selected_tasksize;
 	}
+	list_reset(&ListHead);
 	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	rcu_read_unlock();
@@ -224,11 +323,32 @@ static struct shrinker lowmem_shrinker = {
 	.seeks = DEFAULT_SEEKS * 16
 };
 
+unsigned int readflag;
+unsigned int readcntflag;
+static ssize_t sigkill_count_proc_read(struct file *filp, char __user *buff, size_t len, loff_t *off)
+{
+    char print_buf[32];
+    unsigned int ret = 0, iret = 0;
+    sprintf(print_buf, "lmk_sigkill_cnt: %d\n", lmk_sigkill_cnt);
+    ret = strlen(print_buf);
+    iret = copy_to_user(buff, print_buf, ret);
+    if (!readcntflag) {
+				readcntflag = 1;
+				return ret;
+		} else {
+				readcntflag = 0;
+				return 0;
+		}
+}
+
+static struct file_operations sigkill_count_proc_ops = {
+    .read = sigkill_count_proc_read,
+};
+
 static int __init lowmem_init(void)
 {
-	is4gDram = Read_TOTAL_DRAM()>3000?1:0;
-	pr_info("DRAM total size = %dM, Is 4G device = %d\n", Read_TOTAL_DRAM(), is4gDram);
 	register_shrinker(&lowmem_shrinker);
+	proc_create("lmk_sigkill_count", S_IRWXUGO, NULL, &sigkill_count_proc_ops);
 	return 0;
 }
 
